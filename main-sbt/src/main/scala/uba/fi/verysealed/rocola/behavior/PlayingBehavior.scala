5deee3901fed62a96e85e8e2a799e8e7d8533511
Camaina <m.stibel@gmail.com>
2024-06-15 16:42:18 -0300

    Adding DirectoryHttpServer missing functionality adding routes for adding, removing and listing playlists and songs

--- main-sbt/src/main/scala/uba/fi/verysealed/rocola/behavior/PlayingBehavior.scala
+++ main-sbt/src/main/scala/uba/fi/verysealed/rocola/behavior/PlayingBehavior.scala
@@ -1,61 +1,40 @@
 package uba.fi.verysealed.rocola.behavior
 
-import akka.Done
-import akka.actor.typed.scaladsl.adapter.TypedActorSystemOps
-import akka.actor.{Actor, ActorSystem}
-import akka.actor.typed.{ActorRef, Behavior, Props}
 import akka.actor.typed.scaladsl.{AbstractBehavior, ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
 import akka.stream.Materializer
-import akka.stream.scaladsl.Source
-import uba.fi.verysealed.rocola.RocolaManager.PlaySessionCommand
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
 import uba.fi.verysealed.rocola.RocolaManager._
 import uba.fi.verysealed.rocola.behavior.playlist.SongMetadataImporter.importSongsFromJson
+import uba.fi.verysealed.rocola.behavior.playlist._
 
-import scala.concurrent.duration.DurationInt
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
-case class SongMetadata(title: String, artist: String, genre: String, duration: Int)
+object Listener {
+  def apply(): Behavior[SongPlaybackEvent] = Behaviors.receive { (context, message) =>
+    message match {
+      case NewSongStarted(song) =>
+        context.log.info(s"Listener received: New song '${song.title}' by ${song.artist} has started.")
+        Behaviors.same
+
+      case SongHasFinished(songTitle) =>
+        context.log.info(s"Listener received: Song '$songTitle' has finished.")
+        Behaviors.same
+      case SongEnqueued(song) =>
+        context.log.info(s"Listener received: Song '${song.title}' by ${song.artist} has been enqueued.")
+        Behaviors.same
+    }
+  }
+}
+case class SongMetadata(title: String, artist: String, genre: String, duration: Int, votes:Int = 0)
+// Define the JSON format for SongMetadata
+object SongMetadata {
+  implicit val format: RootJsonFormat[SongMetadata] = jsonFormat5(SongMetadata.apply)
+}
 
 class PlayingBehavior(context: ActorContext[RocolaCommand]
-                               ) extends AbstractBehavior[RocolaCommand](context) {
-
-
-  case class SongHasFinished(songTitle: String)
-
-  object Listener {
-    def apply(): Behavior[SongHasFinished] = Behaviors.receive { (context, message) =>
-      message match {
-        case SongHasFinished(songTitle) =>
-          context.log.info(s"Listener received: Song '$songTitle' has finished.")
-          Behaviors.same
-      }
-    }
-  }
-  // Class to manage song playback
-  class SongPlayer {
-    private var songSources: List[Source[String, _]] = List.empty
-
-    def addSong(song: SongMetadata): Unit = {
-      songSources = songSources :+ createSongSource(song)
-    }
-
-    def playAllSongs(): Future[Done] = {
-      val concatenatedSource = songSources.reduce((acc, src) => acc.concat(src))
-      concatenatedSource.runForeach(message => println(message))
-    }
-  }
-
-  // Helper function to format elapsed time in minutes and seconds
-  def formatTime(seconds: Int): String = {
-    val minutes = seconds / 60
-    val remainingSeconds = seconds % 60
-    f"$minutes%02d:$remainingSeconds%02d"
-  }
-
-  // Method to search for a song by title and artist
-  def searchSong(title: String, artist: String): Option[SongMetadata] = {
-    songs.find(song => song.title.equalsIgnoreCase(title) && song.artist.equalsIgnoreCase(artist))
-  }
+                     ) extends AbstractBehavior[RocolaCommand](context) {
 
   implicit val ec: ExecutionContext = context.system.executionContext
   implicit val materializer: Materializer = Materializer(context.system)
@@ -64,44 +43,209 @@ class PlayingBehavior(context: ActorContext[RocolaCommand]
   println(s"Imported songs: $songs")
   // Create the classic ActorSystem from the typed ActorSystem
   // Create the Listener actor
-  val listener: ActorRef[SongHasFinished] = context.spawn(Listener(), "listener")
+  val listener: ActorRef[SongPlaybackEvent] = context.spawn(Listener(), "listener")
+  val songsPlayer = new SongPlayer(context, listener)
 
- val songsPlayer = new SongPlayer()
+  // Method to search for a song by title and artist
+  def searchSong(title: String, artist: String): Option[SongMetadata] = {
+    songs.find(song => song.title.equalsIgnoreCase(title) && song.artist.equalsIgnoreCase(artist))
+  }
+  override def onMessage(msg: RocolaCommand): Behavior[RocolaCommand] = {
+    msg match {
+      case SendPlay(replyTo) =>
+        try {
+          val result = songsPlayer.resumeCurrentSong()
+          val message = if (result) "Playing has been started!" else "There is nothing to play!"
+          replyTo ! PlaybackResponse(success = result, message = message)
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaybackResponse(success = false, message = s"Failed to play: ${e.getMessage}")
+        }
+        this
 
+      case SendPause(replyTo) =>
+        try {
+          val result = songsPlayer.stopCurrentSong()
+          val message = if (result) "Successfully paused" else "Player was already paused."
+          replyTo ! PlaybackResponse(success = result, message = message)
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaybackResponse(success = false, message = s"Failed to pause: ${e.getMessage}")
+        }
+        this
 
-   // Create a Source that emits elapsed time messages for a song and a final message when the song finishes
-   def createSongSource(song: SongMetadata): Source[String, _] = {
-     val tickSource = Source.tick(0.seconds, 5.seconds, 5)
-       .scan(0)((counter, step) => counter + step) // Increment the counter
-       .takeWhile(_ <= song.duration) // Stop the source when the counter reaches 4 minutes
-       .map(seconds => s"Playing song $song.title from artists ${song.artist} elapsed ${formatTime(seconds)}") // Format the message
+      case SendStop(replyTo) =>
+        try {
+          val result = songsPlayer.stopCurrentSong()
+          val message = if (result) "Successfully stopped" else "Player was already stopped."
+          replyTo ! PlaybackResponse(success = result, message = message)
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaybackResponse(success = false, message = s"Failed to stop: ${e.getMessage}")
+        }
+        this
 
-     val endOfSongMessage = Source.single {
-       listener ! SongHasFinished(song.title) // Send a message to the Listener actor
-       s"Finished playing song $song.title"
-     }
+      case SendSkipSong(replyTo) =>
+        try {
+          songsPlayer.skipCurrentSong()
+          replyTo ! PlaybackResponse(success = true, message = "Skipped")
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaybackResponse(success = false, message = s"Failed to skip: ${e.getMessage}")
+        }
+        this
 
-     tickSource.concat(endOfSongMessage) // Concatenate the end-of-song message source
-   }
+      case SendVolumeUp(replyTo) =>
+        try {
+          songsPlayer.increaseVolume(10)
+          replyTo ! VolumeControlResponse(success = true, message = "Volume increased")
+        } catch {
+          case e: Exception =>
+            replyTo ! VolumeControlResponse(success = false, message = s"Failed to increase volume: ${e.getMessage}")
+        }
+        this
 
-    override def onMessage(msg: RocolaCommand): Behavior[RocolaCommand] = {
-      msg match {
-        case Play() =>
-          songsPlayer.playAllSongs()
-          this
-        case EnqueueSong(title:String,artist:String, replyTo: ActorRef[EnqueueSongResponse]) =>
-          // Example search
-          val searchedSong = searchSong(title,artist)
+      case SendVolumeDown(replyTo) =>
+        try {
+          songsPlayer.decreaseVolume(10)
+          replyTo ! VolumeControlResponse(success = true, message = "Volume decreased")
+        } catch {
+          case e: Exception =>
+            replyTo ! VolumeControlResponse(success = false, message = s"Failed to decrease volume: ${e.getMessage}")
+        }
+        this
+
+      case SendMute(replyTo) =>
+        try {
+          songsPlayer.mute()
+          replyTo ! VolumeControlResponse(success = true, message = "Muted")
+        } catch {
+          case e: Exception =>
+            replyTo ! VolumeControlResponse(success = false, message = s"Failed to mute: ${e.getMessage}")
+        }
+        this
+
+      case SendUnmute(replyTo) =>
+        try {
+          songsPlayer.unMute()
+          replyTo ! VolumeControlResponse(success = true, message = "Unmuted")
+        } catch {
+          case e: Exception =>
+            replyTo ! VolumeControlResponse(success = false, message = s"Failed to unmute: ${e.getMessage}")
+        }
+        this
+
+      case SetVolume(volume, replyTo) =>
+        try {
+          songsPlayer.setVolume(volume)
+          replyTo ! VolumeControlResponse(success = true, message = s"Volume set to $volume")
+        } catch {
+          case e: Exception =>
+            replyTo ! VolumeControlResponse(success = false, message = s"Failed to set volume: ${e.getMessage}")
+        }
+        this
+
+      case AskPlaylist(replyTo) =>
+        try {
+          replyTo ! PlaylistResponse(success = true, message = "Playlist obtained", playlist = songsPlayer.getCurrentPlaylist)
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaylistResponse(success = false, message = s"Failed to get playlist: ${e.getMessage}", playlist = songsPlayer.getCurrentPlaylist)
+        }
+        this
+
+      case SetPlaylist(playlist, replyTo) =>
+        try {
+          val result = songsPlayer.addNewPlaylist(playlist)
+          replyTo ! PlaylistResponse(success = result, message = "Playlist replaced", playlist = playlist)
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaylistResponse(success = false, message = s"Failed to set playlist: ${e.getMessage}", playlist = playlist)
+        }
+        this
+
+      case DequeueSong(title, artist, replyTo) =>
+        try {
+          val searchedSong = searchSong(title, artist)
           searchedSong match {
             case Some(song) =>
               println(s"Found song: $song")
-              songsPlayer.addSong(song)
-              replyTo ! EnqueueSongResponse(success = true)
+              songsPlayer.removeSong(title, artist)
+              replyTo ! PlaylistResponse(
+                success = true,
+                message = s"Song '$title' by $artist has been removed",
+                playlist = songsPlayer.getCurrentPlaylist)
             case None =>
               println("Song not found")
-              replyTo ! EnqueueSongResponse(success = false)
+              replyTo ! PlaylistResponse(
+                success = false,
+                message = s"Song not found: $title by $artist",
+                playlist = songsPlayer.getCurrentPlaylist)
           }
-          this
-      }
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaylistResponse(
+              success = false,
+              message = s"Failed when trying to remove song from queue: $title by $artist. Error: ${e.getMessage}",
+              playlist = songsPlayer.getCurrentPlaylist)
+        }
+        this
+
+      case EnqueueSong(title: String, artist: String, votes: Integer, replyTo) =>
+        try {
+          val searchedSong = searchSong(title, artist)
+          searchedSong match {
+            case Some(song) =>
+              println(s"Found song: $song")
+              val result = songsPlayer.addSong(song)
+              val message = if (result) s"Successfully added song: $title by $artist" else s"Song already in queue: $title by $artist"
+              replyTo ! PlaylistResponse(result, message, playlist = songsPlayer.getCurrentPlaylist)
+            case None =>
+              println("Song not found")
+              replyTo ! PlaylistResponse(
+                success = false,
+                s"Specified song has not included into the library yet: $title by $artist",
+                playlist = songsPlayer.getCurrentPlaylist)
+          }
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaylistResponse(
+              success = false,
+              message = s"Failed when trying to add a song to the queue: $title by $artist. Error: ${e.getMessage}",
+              playlist = songsPlayer.getCurrentPlaylist)
+        }
+        this
+      case DequeueSongByOrdinal(ordinal, replyTo) =>
+        try {
+          songsPlayer.removeSongByPosition(ordinal)
+          val message =s"Song with ordinal $ordinal has been removed"
+          replyTo ! PlaylistResponse(success = true, message, playlist = songsPlayer.getCurrentPlaylist)
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaylistResponse(success = false, message = s"Failed to remove song with ordinal $ordinal: ${e.getMessage}", playlist = songsPlayer.getCurrentPlaylist)
+        }
+        this
+      case VoteSong(votePositive, title, artist, replyTo) =>
+        try {
+          val quantity = if (votePositive) 1 else -1
+          songsPlayer.updateVotes(title, artist, quantity)
+          replyTo ! PlaylistResponse(success = true, message = s"Vote for song $title by $artist has been updated", playlist = songsPlayer.getCurrentPlaylist)
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaylistResponse(success = false, message = s"Failed to update votes for song $title by $artist: ${e.getMessage}", playlist = songsPlayer.getCurrentPlaylist)
+        }
+        this
+
+      case VoteSongByOrdinal(votePositive, ordinal, replyTo) =>
+        try {
+          val quantity = if (votePositive) 1 else -1
+          songsPlayer.updateVotesByPosition(ordinal, quantity)
+          replyTo ! PlaylistResponse(success = true, message = s"Vote for song with ordinal $ordinal has been updated", playlist = songsPlayer.getCurrentPlaylist)
+        } catch {
+          case e: Exception =>
+            replyTo ! PlaylistResponse(success = false, message = s"Failed to update votes for song with ordinal $ordinal: ${e.getMessage}", playlist = songsPlayer.getCurrentPlaylist)
+        }
+        this
     }
   }
+}
---
+++ main-sbt/src/main/scala/uba/fi/verysealed/rocola/behavior/playlist/SongPlayer.scala
@@ -0,0 +1,244 @@
+package uba.fi.verysealed.rocola.behavior.playlist
+
+import akka.Done
+import akka.actor.typed.ActorRef
+import akka.actor.typed.scaladsl.ActorContext
+import akka.stream.{KillSwitches, Materializer}
+import akka.stream.scaladsl.{Keep, Source}
+import uba.fi.verysealed.rocola.RocolaManager.RocolaCommand
+import uba.fi.verysealed.rocola.behavior.SongMetadata
+
+import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger}
+import scala.collection.mutable
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
+import scala.collection.mutable.PriorityQueue
+
+sealed trait SongPlaybackEvent
+final case class SongHasFinished(songTitle: String) extends SongPlaybackEvent
+final case class NewSongStarted(song: SongMetadata) extends SongPlaybackEvent
+final case class CurrentSongEnded(song: SongMetadata) extends SongPlaybackEvent
+final case class CurrentSongStopped(song: SongMetadata) extends SongPlaybackEvent
+final case class CurrentSongResumed(song: SongMetadata) extends SongPlaybackEvent
+final case class CurrentSongSkipped(song: SongMetadata) extends SongPlaybackEvent
+final case class SongEnqueued(song: SongMetadata) extends SongPlaybackEvent
+final case class PlaybackStatusChanged(message: String) extends SongPlaybackEvent
+final case class PlaylistStatusChanged(message: String) extends SongPlaybackEvent
+
+class SongPlayer(context: ActorContext[RocolaCommand], listener: ActorRef[SongPlaybackEvent]) {
+  implicit val ec: ExecutionContext = context.system.executionContext
+  implicit val materializer: Materializer = Materializer(context.system)
+
+  private var songQueue: mutable.PriorityQueue[SongMetadata] = mutable.PriorityQueue.empty(Ordering.by(_.votes)) // Default ordering by votes
+  private var currentSong: Option[SongMetadata] = None
+  private var elapsedTime: Option[AtomicInteger] = None
+
+  private var currentSongSource: Option[akka.stream.scaladsl.Source[String, _]] = None
+  private var songSourceControl: Option[akka.stream.KillSwitch] = None
+
+  private val minVolume = 0
+  private val maxVolume = 100
+  private val volume: AtomicInteger = new AtomicInteger(50) // Default volume is 50
+  private val muted: AtomicBoolean = new AtomicBoolean(false)
+
+  def increaseVolume(step: Int): Unit = {
+    val newVolume = Math.min(maxVolume, volume.addAndGet(step))
+    volume.set(newVolume)
+    listener ! PlaybackStatusChanged(s"Volume increased to $newVolume")
+  }
+
+  def mute(): Unit = {
+    muted.set(true)
+    listener ! PlaybackStatusChanged("Volume muted")
+  }
+
+  def unMute(): Unit = {
+    muted.set(false)
+    listener ! PlaybackStatusChanged("Volume unmuted")
+  }
+  def skipCurrentSong(): Unit = {
+    songSourceControl.foreach(_.shutdown())
+    currentSong match {
+      case Some(song) =>
+        listener ! CurrentSongSkipped(song)
+        listener ! PlaybackStatusChanged(s"Skipped song '${song.title}' by '${song.artist}'")
+        currentSong = None
+        songSourceControl = None
+        playNextSong()
+      case None =>
+        listener ! PlaybackStatusChanged("No song is currently playing to skip")
+    }
+  }
+  def decreaseVolume(step: Int): Unit = {
+    val newVolume = Math.max(minVolume, volume.addAndGet(-step))
+    volume.set(newVolume)
+    listener ! PlaybackStatusChanged(s"Volume decreased to $newVolume")
+  }
+  // Helper function to format elapsed time in minutes and seconds
+  private def formatTime(seconds: Int): String = {
+    val minutes = seconds / 60
+    val remainingSeconds = seconds % 60
+    f"$minutes%02d:$remainingSeconds%02d"
+  }
+
+  private def createSongSource(song: SongMetadata, startFrom: Int = 0): Source[String, _] = {
+    val elapsed = new AtomicInteger(startFrom)
+    elapsedTime = Some(elapsed)
+
+    val (killSwitch, tickSource) = Source.tick(0.seconds, 5.seconds, 5)
+      .scan(startFrom)((counter, step) => counter + step) // Increment the counter
+      .takeWhile(_ <= song.duration) // Stop the source when the counter reaches the song duration
+      .map { seconds =>
+        elapsed.set(seconds)
+        val message = s"Playing song ${song.title} by ${song.artist}, elapsed ${formatTime(seconds)}"
+        listener ! PlaybackStatusChanged(message)
+        message
+      } // Format the message and send playback status
+      .viaMat(KillSwitches.single)(Keep.right)
+      .preMaterialize()
+
+    songSourceControl = Some(killSwitch)
+
+    val endOfSongMessage = Source.single {
+      listener ! SongHasFinished(song.title) // Send a message to the Listener actor
+      s"Finished playing song ${song.title}"
+    }
+
+    currentSongSource = Some(tickSource.concat(endOfSongMessage)) // Concatenate the end-of-song message source
+    tickSource.concat(endOfSongMessage)
+  }
+
+  def stopCurrentSong(): Boolean = {
+    songSourceControl.foreach(_.shutdown())
+    currentSong match {
+      case Some(song) =>
+        listener ! CurrentSongStopped(song)
+        listener ! PlaybackStatusChanged(s"Stopped playing song '${song.title}' by '${song.artist}'")
+        currentSong = None
+        songSourceControl = None
+        true
+      case None =>
+        listener ! PlaybackStatusChanged("No song is currently playing")
+        false
+    }
+  }
+
+  def setVolume(volumeLevel: Int): Unit = {
+    if (volumeLevel < minVolume || volumeLevel > maxVolume) {
+      throw new OutOfBoundException(s"Volume level $volumeLevel is out of bounds (must be between $minVolume and $maxVolume)")
+    } else {
+      volume.set(volumeLevel)
+      listener ! PlaybackStatusChanged(s"Volume set to $volumeLevel")
+    }
+  }
+
+  def resumeCurrentSong(): Boolean = {
+    currentSong match {
+      case Some(song) if elapsedTime.isDefined =>
+        val startFrom = elapsedTime.get.get()
+        listener ! CurrentSongResumed(song)
+        listener ! PlaybackStatusChanged(s"Resuming song '${song.title}' by '${song.artist}' from ${formatTime(startFrom)}")
+
+        val songSource = createSongSource(song, startFrom)
+        songSource.runForeach(message => println(message)).onComplete { _ =>
+          currentSong = None
+          listener ! CurrentSongEnded(song)
+          playNextSong()
+        }
+        true
+      case _ =>
+        listener ! PlaybackStatusChanged("No song to resume, trying to play next song, if defined...")
+        playNextSong()
+    }
+  }
+
+  def addSong(song: SongMetadata): Boolean = {
+    songQueue.enqueue(song)
+    listener ! SongEnqueued(song)
+    true
+  }
+
+  private def playNextSong(): Boolean = {
+    if (currentSong.isEmpty && songQueue.nonEmpty) {
+      val song = songQueue.dequeue()
+      currentSong = Some(song)
+      listener ! NewSongStarted(song)
+
+      val songSource = createSongSource(song)
+      songSource.runForeach(message => println(message)).onComplete { _ =>
+        currentSong = None
+        listener ! CurrentSongEnded(song)
+        playNextSong()
+      }
+      true
+    }
+    else
+      false
+  }
+
+  private def rankQueue(ranking: (SongMetadata, SongMetadata) => Boolean): Unit = {
+    songQueue = mutable.PriorityQueue(songQueue.toSeq: _*)(Ordering.fromLessThan(ranking))
+  }
+
+
+  def updateVotes(songTitle: String, artist: String, quantity: Int): Unit = {
+    val songOpt = songQueue.find(song => song.title == songTitle && song.artist == artist)
+    songOpt match {
+      case Some(song) =>
+        songQueue = songQueue.filterNot(_ == song)
+        val updatedVotes = math.max(0, song.votes + quantity)
+        val updatedSong = song.copy(votes = updatedVotes)
+        songQueue.enqueue(updatedSong)
+        listener ! PlaylistStatusChanged(s"Votes for song '${song.title}' by '${song.artist}' updated to $updatedVotes")
+        rankQueue((a, b) => a.votes > b.votes) // Re-rank the queue
+      case None =>
+        listener ! PlaylistStatusChanged(s"Song '$songTitle' by '$artist' not found in queue")
+    }
+  }
+  def updateVotesByPosition(position: Int, quantity: Int): Unit = {
+    if (position < 0 || position >= songQueue.size) {
+      listener ! PlaybackStatusChanged(s"Invalid position: $position")
+      return
+    }
+    val songs = songQueue.toList
+    val song = songs(position)
+    songQueue = mutable.PriorityQueue(songs.filterNot(_ == song): _*)(Ordering.by(_.votes))
+    val updatedVotes = math.max(0, song.votes + quantity)
+    val updatedSong = song.copy(votes = updatedVotes)
+    songQueue.enqueue(updatedSong)
+    listener ! PlaybackStatusChanged(s"Votes for song '${song.title}' by '${song.artist}' updated to $updatedVotes")
+  }
+
+  def removeSong(title: String, artist: String): Unit = {
+    val songOpt = songQueue.find(song => song.title == title && song.artist == artist)
+    songOpt match {
+      case Some(song) =>
+        songQueue = songQueue.filterNot(_ == song)
+        listener ! PlaybackStatusChanged(s"Removed song '${song.title}' by '${song.artist}' from the queue")
+      case None =>
+        listener ! PlaybackStatusChanged(s"Song '$title' by '$artist' not found in queue")
+    }
+  }
+  def removeSongByPosition(position: Int): Unit = {
+    if (position < 0 || position >= songQueue.size) {
+      listener ! PlaybackStatusChanged(s"Invalid position: $position")
+      return
+    }
+    val songs = songQueue.toList
+    val song = songs(position)
+    songQueue = mutable.PriorityQueue(songs.filterNot(_ == song): _*)(Ordering.by(_.votes))
+    listener ! PlaybackStatusChanged(s"Removed song '${song.title}' by '${song.artist}' from the queue at position $position")
+  }
+
+  def getCurrentPlaylist: List[SongMetadata] = {
+    songQueue.toList
+  }
+
+  def addNewPlaylist(songs: List[SongMetadata]): Boolean = {
+    songQueue.clear()
+    songs.foreach(songQueue.enqueue(_))
+    rankQueue((a, b) => a.votes > b.votes) // Order the queue by votes
+    listener ! PlaybackStatusChanged("New playlist added and ordered by votes")
+    true
+  }
+}
