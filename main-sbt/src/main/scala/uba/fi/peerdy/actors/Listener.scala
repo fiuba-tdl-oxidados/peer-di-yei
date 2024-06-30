9e21a590130295395464d4919a9359a8e1174021
earagon04 <earagon@fi.uba.ar>
2024-06-30 19:04:31 -0300

    prints consola

--- main-sbt/src/main/scala/uba/fi/peerdy/actors/Listener.scala
+++ main-sbt/src/main/scala/uba/fi/peerdy/actors/Listener.scala
@@ -1,12 +1,9 @@
 package uba.fi.peerdy.actors
 
-import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.Behaviors
+import uba.fi.peerdy.actors.DiYei.{ProcessCommand, ShowCommands}
 import uba.fi.peerdy.actors.rocola.Rocola
 import uba.fi.peerdy.actors.Member
 import uba.fi.peerdy.actors.protocol.PeerProtocol
-import scala.concurrent.{ExecutionContextExecutor, Future, Promise}
-import scala.util.{Failure, Success}
 
 object Listener {
   sealed trait ListenerCommand
@@ -34,26 +31,47 @@ object Listener {
       Behaviors.receiveMessage {
         case ShowCommands() =>
           println("Available commands:")
-          println("exit - Exit the system")
+          println("commands | List available commands")
+          println("play | Play playlist")
+          println("pause | Pause playlist")
+          println("skip | Skip to next song")
+          println("list | List the playlist")
+          println("volumeup | Adds 1 to the current volume")
+          println("volumedown | Takes away 1 to the current volume")
+          println("volume N | Set the volume to N")
+          println("mute | Mutes")
+          println("unmute | Unmutes")
+          println("vote <song> <artist> | Pause playlist")
+          println("exit | Exit the system")
           Behaviors.same
-        case ProcessCommand(cmd) =>
+        case ProcessCommand(msg) =>
+          val args = msg.split(" ")
+          val cmd = args(0)
           cmd match {
             case "commands" =>
               context.self ! ShowCommands()
             case "play" =>
               rocola ! Rocola.Play()
-            case "ping" =>
-              peerProtocol ! PeerProtocol.SendMessage(s"Ping del listener $port")
-            case "present" =>
-              peerProtocol ! PeerProtocol.SendMessage(s"NL-$address-$port")
             case "pause" =>
               rocola ! Rocola.Pause()
-            case "propose" =>
-              println("Enter song name: ")
-              val song = scala.io.StdIn.readLine()
-              println("Enter artist name: ")
-              val artist = scala.io.StdIn.readLine()
-              rocola ! Rocola.EnqueueSong(song, artist)
+            case "skip" =>
+              rocola ! Rocola.Skip()
+            case "list" =>
+              rocola ! Rocola.List()
+            case "volumeup" =>
+              rocola ! Rocola.VolumeUp()
+            case "volumedown" =>
+              rocola ! Rocola.VolumeDown()
+            case "volume" =>
+              rocola ! Rocola.SetVolume(args(1).toInt)
+            case "mute" =>
+              rocola ! Rocola.Mute()
+            case "unmute" =>
+              rocola ! Rocola.Unmute()
+            case "vote" =>
+              rocola ! Rocola.EnqueueSong(args(1), args(2))
+            case "do" =>
+              peerProtocol ! PeerProtocol.SendMessage("Ping del diYei")
             case _ =>
               context.log.info("Invalid command")
           }
